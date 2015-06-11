6d562d3584bd671abb7003027aaf2bb3360ebcab
AmitaiB <blickstein@gmail.com>
2015-06-11 18:20:14 -0400

    fails with for-in loop

--- FiboFinder/FISFiboFinder.m
+++ FiboFinder/FISFiboFinder.m
@@ -17,20 +17,25 @@
 //    Fn2 = Fn1 + Fn2
 //    [...]
 //    FnN = Fn(N-1)+(Fn(N-2)
-    NSMutableArray *fibSequence = [NSMutableArray arrayWithCapacity:index];
+    NSMutableArray *fibSequence = [@[]mutableCopy];
     
-    fibSequence[0] = @0;
-    fibSequence[1] = @1;
+//    fibSequence[0] = @0;
+//    fibSequence[1] = @1;
+    [fibSequence addObject:@0];
+    [fibSequence addObject:@1];
     
-    [fibSequence enumerateObjectsUsingBlock:^(NSNumber *obj, NSUInteger idx, BOOL *stop) {
-        if (idx > 1) {
-            NSUInteger fibIntIdxMinus1 = [fibSequence[idx - 1] intValue];
-            NSUInteger fibIntIdxMinus2 = [fibSequence[idx - 2] intValue];
-            
-            NSUInteger fibIntIdx       = fibIntIdxMinus1 + fibIntIdxMinus2;
-            fibSequence[idx] = [NSNumber numberWithUnsignedInteger:fibIntIdx];
+    for (NSUInteger idx = 3; idx <= index; idx++) {
+        [fibSequence addObject:[NSNumber numberWithUnsignedInteger:([fibSequence[idx - 1] intValue] + [fibSequence[idx - 2] intValue])]];
+        NSLog(@"fibSequence[%d] = %@", fibSequence[idx]);
+        
+//            NSUInteger fibIntIdxMinus1 = [fibSequence[idx - 1] intValue];
+//            NSUInteger fibIntIdxMinus2 = [fibSequence[idx - 2] intValue];
+//            
+//            NSUInteger fibIntIdx       = fibIntIdxMinus1 + fibIntIdxMinus2;
+//            fibSequence[idx] = [NSNumber numberWithUnsignedInteger:fibIntIdx];
         }
-    }];
+   
+    NSLog(@"For index: %i, produced number %@", index, fibSequence[index]);
     return [fibSequence[index] intValue];
 
     
